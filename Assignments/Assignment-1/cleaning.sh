9f7df789ab70f1d405edb959546b4ce3f4f8b915
Anne Tissera <anne.tissera@uoit.net>
2015-11-12 12:29:54 -0500

    comitting Assignment-1

---
+++ Assignments/Assignment-1/cleaning.sh
@@ -0,0 +1,29 @@
+  466  find . -name 'NOTES' -print
+  467  rm ./data/jamesm/NOTES
+  468  rm ./data/Frank_Richard/NOTES
+  469  pwd
+  470  ls
+  471  mkdir cleaned_data
+  472  ls
+  473  cp -r data cleaned_data
+  474  cd cleaned_data
+  475  ls
+  476  cd data
+  477  ls
+  478  rm alexander alexander.txt
+  479  cd alexander
+  480  ls
+  481  rm alexander alexander.txt
+  482  cd ..
+  483  pwd
+  484  ls
+  485  rm alexander alexander.txt
+  486  mv alexander alexander.txt
+  487  mv Bert Bert.txt
+  488  mv Frank_Richard Frank_Richard.txt
+  489  mv gerdal gerdal.txt
+  490  mv jamesm jamesm.txt
+  491  mv Lawrence Lawrence.txt
+  492  mv THOMAS THOMAS.txt
+  493  history
+  
